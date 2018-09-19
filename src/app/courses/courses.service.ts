5212ff40b5c0afe38a5d3afe87e5703cf565e96a
Ronaldo Umana <ronaldo.ur@gmail.com>
2018-09-18 23:44:55 -0600

    Implemented localStorage

--- src/app/courses/courses.service.ts
+++ src/app/courses/courses.service.ts
@@ -2,8 +2,12 @@ import { Injectable } from '@angular/core'
 
 @Injectable()
 export class CoursesService {
+   constructor() {
+      localStorage.setItem('courses', JSON.stringify(Courses))
+   }
+
    getCourses() {
-      return Courses
+      return JSON.parse(localStorage.getItem('courses'))
    }
 }
 
